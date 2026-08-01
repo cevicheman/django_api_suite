fc0ffb78d108782a484de317bb07f1c520921715
Jack Vera <jackvera@espol.edu.ec>
2026-08-01 01:18:59 +0000

    correccion de la guia 20

--- backend_data_server/urls.py
+++ backend_data_server/urls.py
@@ -17,10 +17,9 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
-from templates.homepage import views as homepage_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", homepage_views.index, name="home"),
+    path("", include("homepage.urls")),
     path("homepage/", include("homepage.urls")),
 ]
